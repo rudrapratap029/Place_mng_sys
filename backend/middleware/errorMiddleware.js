e14610cf124b6121f89812f076215fa5c1e19e4a
rudrapratap029 <rudrapratap112005@gmail.com>
2026-07-16 21:16:40 +0530

    Adding Global Error Handler

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,6 +1,24 @@
 
 const errorHandler = (error,req,res,next) => {
-  const statusCode = error.statusCode || 500;
+ 
+  // MongoDB Duplicate Key Error
+if (error.code === 11000) {
+  error.statusCode = 400;
+  error.message = "Email already exists";
+   }
+
+
+    // Validation Error
+    if (error.name === "ValidationError") {
+        error.statusCode = 400;
+    }
+    // Invalid MongoDB ObjectId
+if (error.name === "CastError") {
+    error.statusCode = 400;
+    error.message = "Invalid ID";
+}
+     const statusCode = error.statusCode || 500;
+
   res.status(statusCode).json({
     success : false,
     message : error.message || "Internal Server Error"
