8b18b869e4615bebf5d33e6cb27265c8471c5592
rudrapratap029 <rudrapratap112005@gmail.com>
2026-07-18 23:22:43 +0530

    Adding Dashboards

--- backend/server.js
+++ backend/server.js
@@ -7,32 +7,34 @@ const studentRoutes = require("./routes/studentRoutes");
 const companyRoutes = require("./routes/companyRoutes");
 const applicationRoutes = require("./routes/applicationRoutes");
 const authRoutes = require("./routes/authRoutes");
-const errorHandler = require("./middleware/errorMiddleware");
 const dashboardRoutes = require("./routes/dashboardRoutes");
+
+const errorHandler = require("./middleware/errorMiddleware");
+
 const app = express();
 
+// Connect Database
 connectDB();
 
+// Middleware
 app.use(express.json());
-
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/companies", companyRoutes);
 app.use("/api/students", studentRoutes);
 app.use("/api/application", applicationRoutes);
-
-
-// Global Error Handler (always last)
-app.use(errorHandler);
 app.use("/api/dashboard", dashboardRoutes);
 
-
+// Home Route
 app.get("/", (req, res) => {
   res.send("Welcome to Placement Management System");
 });
 
+// Global Error Handler (Always Last)
+app.use(errorHandler);
 
+// Server
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
