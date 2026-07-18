f28ba0f026a85f603ab9d5ba9c986fabe557f835
rudrapratap029 <rudrapratap112005@gmail.com>
2026-07-18 23:29:45 +0530

    preparing for backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,4 @@
-const Admin = require("../models/Admin");
+const Admin = require("../models/admin");
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
